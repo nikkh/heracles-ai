13be63bde53127d10fb24f92df9f73a1dcdc0fc5
nikkh <nick-9979@outlook.com>
2020-11-06 18:09:22 +0000

    Update encrypt_secret.js

--- encrypt_secret.js
+++ encrypt_secret.js
@@ -1,5 +1,4 @@
 var myArgs = process.argv.slice(2);
-console.log('hopefully the passed in parameter was ' + myArgs[0]);
 const sodium = require('tweetsodium')
 const value = "plain-text-secret"
 const key = "OvpKIvDS+cccMJmLxDk49g5KDGyYaXy9zndDK1zzzWY=";
@@ -7,4 +6,4 @@ const keyBytes = Buffer.from(key, 'base64');
 const messageBytes = Buffer.from(value);
 const encryptedBytes = sodium.seal(messageBytes, keyBytes);
 const encrypted = Buffer.from(encryptedBytes).toString('base64')
-return encrypted;
+console.log(encrypted);
