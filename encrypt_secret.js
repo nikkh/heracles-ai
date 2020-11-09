5524cbe9fb60a611b8584909e381a0d0338f1cb6
nick hill <nick-9979@outlook.com>
2020-11-09 14:17:12 +0000

    syntax

--- encrypt_secret.js
+++ encrypt_secret.js
@@ -3,7 +3,7 @@ const sodium = require('tweetsodium')
 const value = myArgs[0];
 console.log(value);
 const key =  myArgs[1];
-console.log(key;
+console.log(key);
 const keyBytes = Buffer.from(key, 'base64');
 const messageBytes = Buffer.from(value);
 const encryptedBytes = sodium.seal(messageBytes, keyBytes);
